b95fbcb54335eb620227d02aaf9580dc961fdacc
firebreather-heart <dtenny95@gmail.com>
2024-07-20 19:09:22 +0100

    done

--- codetutor/config/production.py
+++ codetutor/config/production.py
@@ -4,7 +4,7 @@ import cloudinary.uploader
 import cloudinary.api
 
 DEBUG = False 
-ALLOWED_HOSTS = ['codetutor-asuq.onrender.com', 'www.codetutor-asuq.onrender.com',]
+ALLOWED_HOSTS = ['*']
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -23,19 +23,19 @@ DATABASES = {
     }
 }
 
-CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'Lax'
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_DOMAIN = '.onrender.com'
-SECURE_SSL_REDIRECT = True
-CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://*.onrender.com',
-                        'https://codetutor-asuq.onrender.com'
-                        ]
+# CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_SAMESITE = 'Lax'
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_DOMAIN = '.onrender.com'
+# SECURE_SSL_REDIRECT = True
+# CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://*.onrender.com',
+#                         'https://codetutor-asuq.onrender.com'
+#                         ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 cloudinary.config(
